27cd9cdcb11ec2bd8115f08d14dfa2ee9950f0b0
xinnan01991 <98335127+Xinnan-Alex@users.noreply.github.com>
2022-04-03 05:29:26 +0800

    changed browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import {HashRouter as Router,Routes,Route} from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Curency from './pages/Curency';
 
