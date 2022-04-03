6bfa68353416df6edcc0f693eef7ca53db78bd6d
xinnan01991 <98335127+Xinnan-Alex@users.noreply.github.com>
2022-04-03 20:28:45 +0800

    change index.js react17 code to react18 code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const container = document.getElementById('root');
-
-const root = ReactDOMClient.createRoot(container);
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
